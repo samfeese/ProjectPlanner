7730d7cc0e57462d78fc726d1eb52da805e83d68
Sam Feese <samuelfeese@outlook.com>
2024-01-21 08:33:12 -0500

    Summary Complete, Code started

---
+++ ProjectPlanner/ProjectPlanner/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectPlanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProjectPlanner/ProjectPlanner/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectPlanner
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
